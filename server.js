3d56f07f222494bf4b2baabddd8c39579d20b176
Vinicius Delascio Fortunato <vinidroid@gmail.com>
2018-09-18 20:51:49 +1000

    master remove DB connection

--- server.js
+++ server.js
@@ -2,17 +2,22 @@ const http = require('http');
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8081;
 
-mongoose
-  .connect(`mongodb://node-shop-api:${process.env.MONGO_KEY}@node-shop-cluster-shard-00-00-9weg9.mongodb.net:27017,node-shop-cluster-shard-00-01-9weg9.mongodb.net:27017,node-shop-cluster-shard-00-02-9weg9.mongodb.net:27017/test?ssl=true&replicaSet=node-shop-cluster-shard-0&authSource=admin&retryWrites=true`)
-  // .connect(`mongodb://localhost/node-shop`)
-  .then(() => {
-    console.log('DB connected Successfully');
-    const server = http.createServer(app);
-    return server.listen(port, () => {
-      console.log(`Server running in port ${port}`);
-    });
-  })
-  .catch(err => console.log('Not connected to DB', err));
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log(`Server running in port ${port}`);
+});
+
+// mongoose
+//   .connect(`mongodb://node-shop-api:${process.env.MONGO_KEY}@node-shop-cluster-shard-00-00-9weg9.mongodb.net:27017,node-shop-cluster-shard-00-01-9weg9.mongodb.net:27017,node-shop-cluster-shard-00-02-9weg9.mongodb.net:27017/test?ssl=true&replicaSet=node-shop-cluster-shard-0&authSource=admin&retryWrites=true`)
+//   // .connect(`mongodb://localhost/node-shop`)
+//   .then(() => {
+//     console.log('DB connected Successfully');
+//     const server = http.createServer(app);
+//     return server.listen(port, () => {
+//       console.log(`Server running in port ${port}`);
+//     });
+//   })
+//   .catch(err => console.log('Not connected to DB', err));
 
